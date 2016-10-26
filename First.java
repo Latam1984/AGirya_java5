b1751da813a40105f9c4337475dba7b609d66232
Aleksey <https://github.com/Latam1984/AGirya_java5.git>
2016-10-26 20:44:09 +0300

    Homework 1

--- First.java
+++ First.java
@@ -2,4 +2,21 @@
  * Created by Aleksey on 26.10.2016.
  */
 public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello, it's first homework");
+    }
+
+    public class SecondClass {
+    }
+
+    public class ThirdClass {
+    }
+
+    public class Fourth {
+    }
+
+    public class Fifth {
+    }
 }
+
+
