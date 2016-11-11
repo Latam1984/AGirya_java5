d2ceac67972fc1ebc5ed6ed1b1ee792a848d6050
Aleksey <https://github.com/Latam1984/AGirya_java5.git>
2016-11-11 23:03:55 +0200

    Homework +3.3

---
+++ modul_3/Homework_3_3/Homework_3_3.java
@@ -0,0 +1,58 @@
+package modul_3.Homework_3_3;
+import java.util.Date;
+
+/**
+ * Created by Aleksey on 11.11.2016.
+ *
+ * Класс Course с полями:
+ Date startDate,
+ String name,
+ int hoursDuration,
+ String teacherName.
+ Создайте 2 конструктора с аргументами
+ startDate, name;
+ hourseDuration, name, teacherName.
+
+ Класс Student с полями:
+ String firstName,
+ String lastName,
+ int group,
+ Course[] coursesTaken,
+ int age.
+ С 2 конструкторами с аргументами
+ firstName, lastName, group;
+ lastName, coursesTaken.
+
+ CollegeStudent унаследован от Student.
+ Дополнительные поля: String collegeName, int rating, long id.
+ Создайте 3 конструктора: 2 таких же, как и в Student and один с аргументами - всеми полями класса.
+
+ Class SpecialStudent унаследован от CollegeStudent.
+ Дополнительные поля long secretKey, String email.
+ Создайте 3 конструктора: 2 таких же, как и в CollegeStudent и один с аргументом secretKey.
+
+ Создайте get-, set-методы для всех полей и сделайте их private согласно принципам ООП.
+ Создайте 5 объектов класса Course. Создайте объекты других классов, используя все конструкторы.
+ У вас должно быть 13 объектов в классе Solution.
+
+ */
+public class Homework_3_3 {
+
+
+    Course course = new Course(new Date(), "Java4");
+    Course course1 = new Course ("Java", 40, "Pasha");
+    Course course2 = new Course("Java5", 80, "Andrey");
+    Course course3 = new Course("Java6",120, "Pasha");
+    Course course4 = new Course("Java7", 180, "Andrey, Pasha");
+
+    Course[] courses = {course,course1,course2,course3,course4};
+
+    Student student1 = new Student("Aleks","Girya", 5);
+    Student student2 = new Student("Girya", courses);
+    CollegeStudent collegeStudent = new CollegeStudent("Anton", "Morozov", 7);
+    CollegeStudent collegeStudent1 = new CollegeStudent("Petrov", courses);
+    CollegeStudent collegeStudent2 = new CollegeStudent("Ivan", "Ivanov", 6, "NTU", - 1, 1515);
+    SpecialStudent specialStudent = new SpecialStudent("Ivanov", courses);
+    SpecialStudent specialStudent1 = new SpecialStudent("Aleksandrov","Aleksandr", 6);
+    SpecialStudent specialStudent2 = new SpecialStudent("Anton", "Antipenko", 5, "NTU", 9, 1514,123432156);
+}
---
+++ modul_3/Homework_3_3/SpecialStudent.java
@@ -0,0 +1,42 @@
+package modul_3.Homework_3_3;
+
+/**
+ * Created by Aleksey on 11.11.2016.
+ *
+ *  Class SpecialStudent унаследован от CollegeStudent.
+ Дополнительные поля long secretKey, String email.
+ Создайте 3 конструктора: 2 таких же, как и в CollegeStudent и один с аргументом secretKey.
+ */
+public class SpecialStudent extends CollegeStudent {
+    private long secretKey;
+    private String email;
+
+    public SpecialStudent(String lastName, Course[] coursesTaken) {
+        super(lastName, coursesTaken);
+    }
+
+    public SpecialStudent(String firstName, String lastName, int group) {
+        super(firstName, lastName, group);
+    }
+
+    public SpecialStudent(String firstName, String lastName, int group, String collegeName, int rating, long id, long secretKey) {
+        super(firstName, lastName, group, collegeName, rating, id);
+        this.secretKey = secretKey;
+    }
+
+    public long getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(long secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
