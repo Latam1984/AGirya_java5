0a60f29e6da64cdc58fe7e58384f4a9c65dc44c3
Aleksey <https://github.com/Latam1984/AGirya_java5.git>
2016-11-11 12:38:35 +0200

    Homework 3

---
+++ modul_3/Homework_3_1/Bird.java
@@ -0,0 +1,14 @@
+package modul_3.Homework_3_1;
+
+/**
+ * Created by Aleksey on 10.11.2016.
+ */
+public class Bird {
+    public void sing  (){
+        System.out.println(" I am walking\n" +
+                " I am flying\n" +
+                " I am singing\n" +
+                " I am Bird");
+
+    }
+}
---
+++ modul_3/Homework_3_2/Arithmetic.java
@@ -0,0 +1,13 @@
+package modul_3.Homework_3_2;
+
+/**
+ * Created by Aleksey on 11.11.2016.
+ */
+public class Arithmetic {
+
+
+    public Integer  add (Integer a, Integer b){
+        Integer sum = a + b;
+        return sum;
+    }
+}
