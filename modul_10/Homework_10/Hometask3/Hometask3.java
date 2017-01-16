ed867ae919d37dd0482113f1baad3cb49f1f267a
Aleksey <https://github.com/Latam1984/AGirya_java5.git>
2017-01-16 21:15:51 +0200

    Homework 10

---
+++ modul_10/Homework_10/Hometask3/Hometask3.java
@@ -0,0 +1,17 @@
+package modul_10.Homework_10.Hometask3;
+
+/**
+ * Created by Aleksey on 16.01.2017.
+ */
+public class Hometask3 {
+    public static void main(String[] args) {
+        Object someObject = new Object();
+        someObject = null;
+
+        try {
+            someObject.notify();
+        } catch (NullPointerException n) {
+            System.out.println("Object null!");
+        }
+    }
+}
