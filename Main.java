9ac575b0c3dd0148e50a092052f99d9d70fd730a
Aleksey <https://github.com/Latam1984/AGirya_java5.git>
2017-01-16 15:22:05 +0200

    Homework 10

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Aleksey on 16.01.2017.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        try {
+            Exception exception = new Exception("Some exeption");
+            throw exception;
+        } catch (Exception e) {
+            System.out.println("We catch some exception!");
+        } finally {
+            System.out.println("Some finally block.");
+        }
+    }
+}
